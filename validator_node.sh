601e9b28f6e8e17442169116a354236e89d8fefb
ehsan shariati <ehsan6sha@gmail.com>
2024-01-10 01:12:26 -0500

    update nginx

--- validator_node.sh
+++ validator_node.sh
@@ -242,7 +242,15 @@ server {
     location / {
         proxy_pass http://127.0.0.1:$RPC_PORT; # Replace with the port number of your WebSocket service
         proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
         proxy_set_header Host \$host;
+
+        # WebSocket specific settings
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_read_timeout 86400; # 24 hours timeout
     }
 }
 EOF
