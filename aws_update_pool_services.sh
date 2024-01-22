4f43b8869f7a608c0e2c1b34d40b66eb344ad9ea
ehsan shariati <ehsan6sha@gmail.com>
2024-01-22 11:22:53 -0500

    added update scripts

---
+++ aws_update_pool_services.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+chmod 600 /home/cloudshell-user/functionland.pem
+# Define your regions
+regions=(
+    us-west-1
+    us-west-2
+    af-south-1
+    ap-east-1
+    ap-south-1
+    ap-south-2
+    ap-northeast-3
+    ap-northeast-2
+    ap-southeast-1
+    ap-southeast-2
+    ap-northeast-1
+    ca-central-1
+    eu-central-1
+    eu-west-1
+    eu-west-2
+    eu-south-1
+    eu-west-3
+    eu-north-1
+    eu-central-2
+    eu-south-2
+    me-central-1
+    il-central-1
+    sa-east-1
+)
+
+# Function to process region
+process_region() {
+    local region=$1
+
+    echo "Processing region: $region"
+    # Retrieve the public IP of the instance
+    instance_ip=$(aws ec2 describe-instances --region $region --query 'Reservations[].Instances[?State.Name==`running`].PublicIpAddress' --output text)
+
+    if [ -n "$instance_ip" ]; then
+        echo "Instance IP: $instance_ip"
+        # Define and call zip_and_upload function via SSH
+        ssh_command="function update() {
+            cd /home/ubuntu/automatic-poolnode-creation-script
+            git pull
+            cd ..
+            bash /home/ubuntu/update_pools.sh
+        }
+        update"
+        # SSH Command (This part needs to be run from a system where SSH is possible)
+        ssh -o StrictHostKeyChecking=no -i /home/cloudshell-user/functionland.pem ubuntu@$instance_ip "$ssh_command" &
+    else
+        echo "Failed to retrieve the instance IP address."
+    fi
+}
+
+# Initial loop through each region
+for region in "${regions[@]}"; do
+    process_region "$region"
+    sleep 10
+done
+# Write instance details to file
+echo "Finished"
---
+++ update_pools.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+set -e
+
+# Function to clone and build repositories
+update_and_build() {
+	echo "Updating sugarfunge-api"
+    if [ ! -d "sugarfunge-api" ] || [ -z "$(ls -A sugarfunge-api)" ]; then
+        git clone https://github.com/functionland/sugarfunge-api.git
+    fi
+    cd sugarfunge-api
+    git pull
+    cargo build --release
+    cd ..
+	
+	echo "Updating sugarfunge-node"
+    if [ ! -d "sugarfunge-node" ] || [ -z "$(ls -A sugarfunge-node)" ]; then
+        git clone https://github.com/functionland/sugarfunge-node.git
+    fi
+    cd sugarfunge-node
+    git pull
+    cargo build --release
+    cd ..
+
+	echo "Updating go-fula"
+    if [ ! -d "go-fula" ] || [ -z "$(ls -A go-fula)" ]; then
+        git clone https://github.com/functionland/go-fula.git
+    fi
+    cd go-fula
+    git pull
+    go build -o go-fula ./cmd/blox
+    cd ..
+}
+
+update_and_restart() {
+    # Stop services
+    echo "Stopping services..."
+    sudo systemctl stop sugarfunge-node.service
+    sudo systemctl stop sugarfunge-api.service
+    sudo systemctl stop go-fula.service
+
+    # Get docker images and process them
+    echo "Processing Docker images..."
+    sudo docker images | while read -r repository tag image_id created size
+    do
+        # Skip header line
+        if [[ "$repository" == "REPOSITORY" ]]; then
+            continue
+        fi
+
+        if [[ "$tag" == "<none>" ]]; then
+            # Remove images without a tag
+            echo "Removing image $image_id..."
+            sudo docker rmi "$image_id"
+        else
+            # Pull images with a tag
+            echo "Pulling image $repository:$tag..."
+            sudo docker pull "$repository:$tag"
+        fi
+        sleep 2
+    done
+
+    # Start services
+    echo "Starting services..."
+    sudo systemctl start sugarfunge-node.service
+    sleep 5
+    sudo systemctl start sugarfunge-api.service
+    sleep 5
+    sudo systemctl start go-fula.service
+
+    # Check services status
+    echo "Checking services status..."
+    for service in sugarfunge-node.service sugarfunge-api.service go-fula.service
+    do
+        status=$(sudo systemctl is-active "$service")
+        if [[ "$status" != "active" ]]; then
+            echo "Error: $service is not running!"
+        else
+            echo "$service is running."
+        fi
+        sleep 5
+    done
+}
+
+# Main script execution
+main() {
+    update_and_build
+    sleep 10
+    update_and_restart
+}
